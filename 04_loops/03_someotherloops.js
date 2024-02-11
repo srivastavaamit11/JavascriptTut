a8458fd76d5e7c0f383eeea7ea68e8a3c3e452c0
Amit Srivastava <90235063+srivastavaamit11@users.noreply.github.com>
2024-02-11 13:36:21 +0000

    other loops foreach

--- 04_loops/03_someotherloops.js
+++ 04_loops/03_someotherloops.js
@@ -23,4 +23,77 @@ for (const [key, value] of map) {
 
 // this above forof syntax wont work on the JavascriptObject.
 
- 
+ // forIn loop
+
+ const myObject = {
+    Js : "Javascript",
+    cpp : "C++",
+    rb : "ruby",
+    swift : "swift by apple"
+ }
+
+ for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+ }
+
+const programming = ["js", "rb", "py", "c++", "java"];
+
+for (const key in programming) {
+    console.log(key);       // here we are getting index of array as the key for array are indexes.
+    // (note it is because of forin loop and because we have written "key" inside the forin loop)
+}
+
+
+// maps are not iterable hence we cant use for in loop on them
+
+
+for (const key in map) {
+    console.log(key);     // maps not iterable.
+}
+
+// foreach
+
+const coding = ["java", "ruby", "javascript", "python", "c++"]
+// using normal function
+coding.forEach( function (item) {
+    console.log(item);
+})
+
+// using Arrow function 
+// for each iteration of the loop the callback function gets 3 params item , index, arrlist
+coding.forEach( (item, index, arr) => {
+    console.log(item, index, arr);
+})
+
+// we can also define a new funciton somewhere and pass the reference of the function in the foeeach loop.
+
+const printMe = (item) => {
+    console.log(item);
+}
+coding.forEach(printMe);
+// remember we need to pass reference to the function not call that function here.
+
+
+
+const myCoding = [
+    {
+        languageName : "Python",
+        languageFileName : "py"
+    },
+    {
+        languageName : "java",
+        languageFileName : "java"
+    },
+    {
+        languageName : "ruby",
+        languageFileName : "rb"
+    },
+    {
+        languageName : "Javascript",
+        languageFileName : "js"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+})
